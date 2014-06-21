8ea2abcb3185c2f49c4989f1bf309877f6149504
Curtis McEnroe <programble@gmail.com>
2014-06-21 19:21:33 -0400

    Reformat stores index

--- stores/index.js
+++ stores/index.js
@@ -3,11 +3,11 @@ module.exports = Stores = function(i) {
   return stores[i];
 };
 
-// Order must remain stable, as we index into this array. New stores should be
-// added to the end. When stores are removed they should be replaced by a null.
+// Indices must remain stable. New stores should be added to the end. An empty
+// item should be left in place of a removed store.
 
-stores.push(Stores.gist    = require('./gist'));
-stores.push(Stores.refheap = require('./refheap'));
-stores.push(Stores.sprunge = require('./sprunge'));
-stores.push(Stores.isgd    = require('./isgd'));
-stores.push(Stores.vgd     = require('./vgd'));
+stores[0] = Stores.gist    = require('./gist');
+stores[1] = Stores.sprunge = require('./sprunge');
+stores[2] = Stores.refheap = require('./refheap');
+stores[3] = Stores.isgd    = require('./isgd');
+stores[4] = Stores.vgd     = require('./vgd');
