6252c0d505bb47c027051fc93831c44b68735f9d
Curtis McEnroe <programble@gmail.com>
2014-06-21 20:04:35 -0400

    Add pastebin.com store

--- stores/index.js
+++ stores/index.js
@@ -6,8 +6,9 @@ module.exports = Stores = function(i) {
 // Indices must remain stable. New stores should be added to the end. An empty
 // item should be left in place of a removed store.
 
-stores[0] = Stores.gist    = require('./gist');
-stores[1] = Stores.sprunge = require('./sprunge');
-stores[2] = Stores.refheap = require('./refheap');
-stores[3] = Stores.isgd    = require('./isgd');
-stores[4] = Stores.vgd     = require('./vgd');
+stores[0] = Stores.gist     = require('./gist');
+stores[1] = Stores.sprunge  = require('./sprunge');
+stores[2] = Stores.refheap  = require('./refheap');
+stores[3] = Stores.isgd     = require('./isgd');
+stores[4] = Stores.vgd      = require('./vgd');
+stores[5] = Stores.pastebin = require('./pastebin');
---
+++ stores/pastebin.js
@@ -0,0 +1,40 @@
+var request = require('superagent');
+
+module.exports = {
+  maxChunkSize: 384000,
+
+  URL: 'http://pastebin.com',
+  API_KEY: '8d61067ca91f8e568421a6c49363dc37',
+
+  put: function(buf, done) {
+    request.post(this.URL + '/api/api_post.php')
+      .type('form')
+      .send({
+        api_dev_key: this.API_KEY,
+        api_option: 'paste',
+        api_paste_private: 1, // Unlisted
+        api_paste_code: buf.toString('base64')
+      })
+      .end(function(err, res) {
+        if (err || res.error)
+          done(err || res.error);
+        else if (res.text)
+          done(null, res.text.split('/').reverse()[0]);
+        else
+          done(new Error('no url'));
+      });
+  },
+
+  get: function(id, done) {
+    request.get(this.URL + '/raw.php')
+      .query({ i: id })
+      .end(function(err, res) {
+        if (err || res.error)
+          done(err || res.error);
+        else if (res.text)
+          done(null, new Buffer(res.text, 'base64'));
+        else
+          done(new Error('no paste text'));
+      });
+  }
+};
